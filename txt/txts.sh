6e6977ce2799755750407147fc83dc6c807cf984
cb <cb@iMac-cb.local>
2022-11-28 21:30:26 +0800

    gx

--- txt/txts.sh
+++ txt/txts.sh
@@ -1,4 +1,4 @@
-echo -e $(dirname $0)/txt/webapi/20*/*.txt $(dirname $0)/txt/高考词汇/*.txt $(dirname $0)/txt/六级词汇/*.txt $(dirname $0)/txt/考研词汇/*.txt | tr ' ' '\n' 
+echo -e $(dirname $0)/webapi/20*/*.txt $(dirname $0)/高考词汇/*.txt $(dirname $0)/六级词汇/*.txt $(dirname $0)/考研词汇/*.txt | tr ' ' '\n' 
 #echo -e $(dirname $0)/txt/webapi/20*/*.txt | tr ' ' '\n' 
 #echo $(dirname $0)/txt/高考词汇/*.txt | tr ' ' '\n' 
 #echo $(dirname $0)/txt/9-4.txt 
