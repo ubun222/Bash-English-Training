f0dca1bcacf21dc0d98142c87007bf1c8055b822
ubun222 <925966086@qq.com>
2024-03-05 19:47:35 +0800

    gx

--- 2.0.6.sh
+++ 2.0.6.sh
@@ -1522,14 +1522,14 @@ thesdex="$(printf "$subs" | grep "\"shortDefinitions\"," | awk -F"	" '{print $2}
 syno="$(printf "$subs" | grep "\"synonyms\"," | awk -F"	" '{printf $2}' | sed "s/\"\"/,/g" )"
 
 
-[[  "$thedex" != ""  ]] && printf 释义"$vsnese"."$((sdex+1))": && prepn "$thedex" 8
+[[  "$thedex" != ""  ]] && printf 释义"$vsnese[a-z\.\(\)\<\>\&]$((sdex+1))": && prepn "$thedex" 8
 [[  "$?" -eq 22  ]] && return 0
-[[  "$thee" != ""  ]] && printf 例句"$vsnese"."$((sdex+1))": && prepn "$thee" 8
+[[  "$thee" != ""  ]] && printf 例句"$vsnese[a-z\.\(\)\<\>\&]$((sdex+1))": && prepn "$thee" 8
 [[  "$?" -eq 22  ]] && return 0
-[[  "$thenote" != ""  ]] && printf 笔记"$vsnese"."$((sdex+1))":"\033[3m""$thenote""\033[0m"\\n
-[[  "$thesdex" != ""  ]] && printf 短释"$vsnese"."$((sdex+1))": && prepn "$thesdex" 8
+[[  "$thenote" != ""  ]] && printf 笔记"$vsnese[a-z\.\(\)\<\>\&]$((sdex+1))":"\033[3m""$thenote""\033[0m"\\n
+[[  "$thesdex" != ""  ]] && printf 短释"$vsnese[a-z\.\(\)\<\>\&]$((sdex+1))": && prepn "$thesdex" 8
 [[  "$?" -eq 22  ]] && return 0
-[[  "$syno" != ""  ]] && printf 同义"$vsnese"."$((sdex+1))":"\033[3m""$syno""\033[0m"\\n
+[[  "$syno" != ""  ]] && printf 同义"$vsnese[a-z\.\(\)\<\>\&]$((sdex+1))":"\033[3m""$syno""\033[0m"\\n
 
 else
 [[  "$sdex" -ne 0   ]] && printf "\033[2m$answer1"的词性"$thecate"第"$((senses+1))"个释义有"$sdex"个子释义"\033[0m"\\n
@@ -1836,7 +1836,7 @@ done
 #passs=
 #whereb="${pos1/#*;/""}"
 if [[  "$which" == "zh"  ]] && [[  $vback -ne 1  ]] && [[  "$ascanf" !=  [a-z\.\(\)\<\>\&]  ]];then
-if ([[  $whereb -eq $((COLUMN-2))  ]] ) || ( [[  $((whereb)) -ne $((COLUMN-1)) ]] && [[  $reg2 == " \b\033[1C"  ]] ) || ( [[  $now4 -eq 1  ]]  &&  [[  $wherec -eq $COLUMN  ]]) && now4= ;then
+if ([[  $whereb -eq $((COLUMN-2))  ]] &&  [[  "${scanf:$((${#scanf}-2)):1}" !=  [a-z\.\(\)\<\>\&]  ]]) || ( [[  $((whereb)) -ne $((COLUMN-1)) ]] && [[  $reg2 == " \b\033[1C"  ]] ) || ( [[  $now4 -eq 1  ]]  &&  [[  $wherec -eq $COLUMN  ]]) && now4= ;then
 #needo=
 whereb="${pos1/#*;/""}" && [[  $whereb -eq $((COLUMN))  ]] && ascanf="~$ascanf" && needo=1
 else
@@ -2275,7 +2275,7 @@ thetemp=
 while read -n1 s;do
 if [[  "$s"  ==  [a-z]  ]];then
 [[  "$thetemp" != ""  ]] && thes= && thetemp=
-#[[  "$s"  ==  "."  ]] && s="s"
+#[[  "$s"  ==  [a-z\.\(\)\<\>\&]  ]] && s="s"
 thes="${thes}$s"
 elif [[  "$s"  ==  '.'  ]];then
 [[  "$thetemp" == ""  ]] && [[  "$s"  ==  '.'  ]] && s="s" && thes="${thes}$s" && eval thetemp="\$${thes}" && eval $thes=\"\${thetemp}+\" && continue #fix ... conflict
@@ -2517,9 +2517,6 @@ fi
 #echo ascanf:$ascanf
 
 if [[  "$ascanf" == "$B"  ]]  ;then
-#stty echo
-#printf 22
-#printf "\b"
 ascanf=""
 L="${#scanf}"
 vback=1
@@ -2533,17 +2530,18 @@ L=$((L-1))
 
 reg2=" $Back $Back"
 if [[  $needo -eq 1  ]] && [[  "$now2" -eq 1  ]] ;then 
-[[  "${scanf:$L}"  != "."  ]] && printf " "
-[[  "${scanf:$L}"  == "."  ]] && printf "\033[1C "
+[[  "${scanf:$L}"  != [a-z\.\(\)\<\>\&]  ]] && printf " "
+[[  "${scanf:$L}"  == [a-z\.\(\)\<\>\&]  ]] && printf "\033[1C "
 fi
-[[  "${scanf:$L}"  == "."  ]] && reg2=" \b\033[1C"
+[[  "${scanf:$L}"  == [a-z\.\(\)\<\>\&]  ]] && reg2=" \b\033[1C"
+
 [[  $needo -eq 1  ]] && [[  "$now2" -eq 1  ]] && reg2="$reg2\b"
 
 [[  "$now3" -eq 2  ]]   &&  printf "$Back\033[1C \033[1C" && scanf="${scanf:0:$L}"   && now3= && continue
 
-[[  "$now2" -eq 1  ]]   &&  printf  "$reg2" && scanf="${scanf:0:$L}" && needo=  && now2= && continue
+[[  "$now2" -eq 1  ]]   &&  printf  "$reg2" && scanf="${scanf:0:$L}" && now2=  continue
 
-[[  "$L"  -ge "0"  ]] && [[  "${scanf:$L}"  == "."  ]]  &&  printf  "$Back $Back" && scanf="${scanf:0:$L}" && continue
+[[  "$L"  -ge "0"  ]] && [[  "${scanf:$L}"  == [a-z\.\(\)\<\>\&]  ]]  &&  printf  "$Back $Back" && scanf="${scanf:0:$L}" && continue
 scanf="${scanf:0:$L}"
 #echo $LENGTH
 #echo ${scanf:$L}
